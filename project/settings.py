de17563cdd3b32bd3c7ac8495f612c5f9112ec5a
Adriele A Secundino <dri.leela@gmail.com>
2023-05-15 15:21:57 -0300

    chore: change ALLOWED_HOSTS

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-favulwz7_kl&ri+^#t)p8miu=$tag@=o&b%%s%fr-ghez)2g)z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
