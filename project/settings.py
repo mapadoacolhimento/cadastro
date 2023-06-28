653aefd50554a2be691c1c248091bc9a34212a0f
Adriele A Secundino <dri.leela@gmail.com>
2023-06-28 11:08:13 -0300

    chore: add bonde

--- project/settings.py
+++ project/settings.py
@@ -17,7 +17,7 @@ env = environ.Env(
     DEBUG=(bool, True),
     SECRET_KEY=(
         str,
-        "django-insecure-cx!j1+m*n87=*iq%m8!^$d8tf0%%=muz4lb5bf4p7h8=zpgfe)",
+        "django-insecure-secret-key",
     ),
     ALLOWED_HOSTS=(list, ["*"]),
 )
@@ -31,7 +31,6 @@ environ.Env.read_env(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-favulwz7_kl&ri+^#t)p8miu=$tag@=o&b%%s%fr-ghez)2g)z'
 SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False
