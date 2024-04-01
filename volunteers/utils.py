86646a28f05f9d09a025cbcbd13069aa822f900c
Viviane Dias <vivi@mapa.org.br>
2024-04-01 21:55:09 +0100

    feat(volunteer): formats utils file with linter

--- volunteers/utils.py
+++ volunteers/utils.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.http import JsonResponse
 import requests
 import json
-from .models import Volunteer,VolunteerStatusHistory,VolunteerAvailability
+from .models import Volunteer, VolunteerStatusHistory, VolunteerAvailability
 from .choices import (
     SUPPORT_TYPE,
     OCCUPATION,
@@ -10,12 +10,12 @@ from .choices import (
 from datetime import datetime
 
 from .constants import (
-  LIST_OF_REJECTED,
-  AVAILABLE_VOLUNTEER_CONDITION,
-  ACTIVE_VOLUNTEER_CONDITION,
-  ABSENT_VOLUNTEER_CONDITION,
-  UNETHICAL_VOLUNTEER_CONDITION,
-  REGISTERED_VOLUNTEER_CONDITION
+    LIST_OF_REJECTED,
+    AVAILABLE_VOLUNTEER_CONDITION,
+    ACTIVE_VOLUNTEER_CONDITION,
+    ABSENT_VOLUNTEER_CONDITION,
+    UNETHICAL_VOLUNTEER_CONDITION,
+    REGISTERED_VOLUNTEER_CONDITION,
 )
 
 
@@ -59,22 +59,22 @@ def send_welcome_email(email, name):
 
 
 def get_new_volunteer_condition(current_condition):
-   
+
     if current_condition in ACTIVE_VOLUNTEER_CONDITION:
         return current_condition
-    
+
     if current_condition in UNETHICAL_VOLUNTEER_CONDITION:
         return current_condition
-      
+
     if current_condition in ABSENT_VOLUNTEER_CONDITION:
         return AVAILABLE_VOLUNTEER_CONDITION
 
     return REGISTERED_VOLUNTEER_CONDITION
- 
- 
+
+
 def get_support_type(occupation):
     psi, legal = SUPPORT_TYPE
-    if occupation  == "psychologist":
+    if occupation == "psychologist":
         return psi[0]
     return legal[0]
 
@@ -82,94 +82,87 @@ def get_support_type(occupation):
 def get_offers_online_support(modality_res):
     if modality_res == "on_site":
         return False
-    return True 
+    return True
 
 
 def create_or_update_volunteer_availability(volunteer: Volunteer):
-    
-      volunteer_availability = VolunteerAvailability.objects.update_or_create(
-                volunteer_id=volunteer.id,
-                defaults = { 
-                            'max_matches': volunteer.availability,
-                            'support_type':get_support_type(volunteer.occupation),
-                            'support_expertise':volunteer.fields_of_work,
-                            'offers_online_support':get_offers_online_support(
-                              volunteer.modality
-                            ),
-                            'city':volunteer.city,
-                            'state':volunteer.state,
-                            'offers_libras_support':volunteer.offers_libras_support,
-                            'lat':volunteer.latitude,
-                            'lng':volunteer.longitude
-                }
-            )
-      return volunteer_availability
+
+    volunteer_availability = VolunteerAvailability.objects.update_or_create(
+        volunteer_id=volunteer.id,
+        defaults={
+            "max_matches": volunteer.availability,
+            "support_type": get_support_type(volunteer.occupation),
+            "support_expertise": volunteer.fields_of_work,
+            "offers_online_support": get_offers_online_support(volunteer.modality),
+            "city": volunteer.city,
+            "state": volunteer.state,
+            "offers_libras_support": volunteer.offers_libras_support,
+            "lat": volunteer.latitude,
+            "lng": volunteer.longitude,
+        },
+    )
+    return volunteer_availability
 
 
-def format_phone(phone):  
-  return (
-            phone
-            .replace(" ", "")
-            .replace("(", "")
-            .replace(")", "")
-            .replace("-", "")
-        )
-  
-  
+def format_phone(phone):
+    return phone.replace(" ", "").replace("(", "").replace(")", "").replace("-", "")
+
+
 def get_volunteer_occupation(type_form):
-      psi, legal = OCCUPATION
-      if type_form == "psicologa":
-          return psi[0]
-      return legal[0]
-  
+    psi, legal = OCCUPATION
+    if type_form == "psicologa":
+        return psi[0]
+    return legal[0]
+
 
 def create_or_update_volunteer(form_data):
-        
-    volunteer, created = Volunteer.objects.update_or_create( email = form_data.values["email"],
-            defaults = { 
-                          'occupation': get_volunteer_occupation(form_data.type_form),
-                          'first_name':form_data.values["first_name"],
-                          'last_name':form_data.values["last_name"],
-                          'phone':format_phone(form_data.values["phone"]),
-                          'zipcode':form_data.values["zipcode"].replace("-", ""),
-                          'state':form_data.values["state"],
-                          'city':form_data.values["city"],
-                          'neighborhood':form_data.values["neighborhood"],
-                          'street':form_data.values["street"],
-                          'register_number':form_data.values["document_number"],
-                          'birth_date':datetime.strptime(form_data.values["birth_date"], "%Y-%m-%d"),
-                          'color':form_data.values["color"],
-                          'gender':form_data.values["gender"],
-                          'modality':form_data.values["modality"],
-                          'fields_of_work':form_data.values["fields_of_work"],
-                          'years_of_experience':form_data.values["years_of_experience"],
-                          'availability':form_data.values["availability"],
-                          'offers_libras_support':form_data.values["libras"]
-                        }            
-        )
-    
-    # se é uma voluntária nova 
+
+    volunteer, created = Volunteer.objects.update_or_create(
+        email=form_data.values["email"],
+        defaults={
+            "occupation": get_volunteer_occupation(form_data.type_form),
+            "first_name": form_data.values["first_name"],
+            "last_name": form_data.values["last_name"],
+            "phone": format_phone(form_data.values["phone"]),
+            "zipcode": form_data.values["zipcode"].replace("-", ""),
+            "state": form_data.values["state"],
+            "city": form_data.values["city"],
+            "neighborhood": form_data.values["neighborhood"],
+            "street": form_data.values["street"],
+            "register_number": form_data.values["document_number"],
+            "birth_date": datetime.strptime(form_data.values["birth_date"], "%Y-%m-%d"),
+            "color": form_data.values["color"],
+            "gender": form_data.values["gender"],
+            "modality": form_data.values["modality"],
+            "fields_of_work": form_data.values["fields_of_work"],
+            "years_of_experience": form_data.values["years_of_experience"],
+            "availability": form_data.values["availability"],
+            "offers_libras_support": form_data.values["libras"],
+        },
+    )
+
+    # se é uma voluntária nova
     if created:
-      volunteer.condition = form_data.values["status"]
+        volunteer.condition = form_data.values["status"]
     else:
-      volunteer.condition = get_new_volunteer_condition(volunteer.condition)
-    
+        volunteer.condition = get_new_volunteer_condition(volunteer.condition)
+
     # se a voluntária for psicóloga salva o campo da abordagem
     if "approach" in form_data.values:
-            volunteer.approach = form_data.values["approach"]
-            
-    #se encontrou as coordenadas do endereço atualiza essa informação
-    if  form_data.values["lat"] != "" and form_data.values["lat"] != "" :
-            volunteer.latitude=form_data.values["lat"]
-            volunteer.longitude=form_data.values["lng"]
-    
+        volunteer.approach = form_data.values["approach"]
+
+    # se encontrou as coordenadas do endereço atualiza essa informação
+    if form_data.values["lat"] != "" and form_data.values["lat"] != "":
+        volunteer.latitude = form_data.values["lat"]
+        volunteer.longitude = form_data.values["lng"]
+
     volunteer.save()
-    
+
     volunteer_status_history = VolunteerStatusHistory.objects.create(
-            volunteer_id=volunteer.id,
-            status=volunteer.condition,
-      )
-    if volunteer.condition not in LIST_OF_REJECTED: 
-      create_or_update_volunteer_availability(volunteer)
+        volunteer_id=volunteer.id,
+        status=volunteer.condition,
+    )
+    if volunteer.condition not in LIST_OF_REJECTED:
+        create_or_update_volunteer_availability(volunteer)
 
     return volunteer
