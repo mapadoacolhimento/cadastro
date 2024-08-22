bc7bc76581135a3a4214b911af0f2c9c16050083
Viviane Dias <vivi@mapa.org.br>
2024-08-22 12:13:34 -0300

    chore(newrelic): initializes new relic agent on project root

--- project/wsgi.py
+++ project/wsgi.py
@@ -7,6 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
+import newrelic.agent
+
+newrelic.agent.initialize("newrelic.ini")
+newrelic.agent.register_application()
+
 import os
 
 from django.core.wsgi import get_wsgi_application
